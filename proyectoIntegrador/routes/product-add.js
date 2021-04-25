2e6f308989ad9f630027288e5758b50b7be169ba
fedesilbert <fsilbert@udesa.edu.ar>
2021-04-25 16:04:30 -0300

    reorg

--- proyectoIntegrador/routes/product-add.js
+++ proyectoIntegrador/routes/product-add.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
+var productAddController= require('../controllers/productAddController');
+router.get()
+
+
 module.exports = router;
