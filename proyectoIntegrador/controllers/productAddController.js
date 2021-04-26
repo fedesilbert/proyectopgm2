cb058caee61d0d75b49386de0ab14cd9d85f591d
Santos-Pazo <69361240+Santos-Pazo@users.noreply.github.com>
2021-04-26 12:43:21 -0300

    ...

---
+++ proyectoIntegrador/controllers/productAddController.js
@@ -0,0 +1,8 @@
+const productAddController = {
+    'productAdd': function(req, res, next){
+        res.render('productAdd', {productAdd: 'productAdd'})
+    },
+  
+     
+    }
+    module.exports = productAddController;
--- proyectoIntegrador/controllers/usersController.js
+++ proyectoIntegrador/controllers/usersController.js
@@ -1,10 +1,10 @@
 
 const usersController = {
 'register': function(req, res, next){
-    res.render('register', {register: login})
+    res.render('register', {register: 'register'})
 },
 'login': function(req, res, next){
-    res.render('login', {login: login})
+    res.render('login', {login: 'login'})
  
 }
 
