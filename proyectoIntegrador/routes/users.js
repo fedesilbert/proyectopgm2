3f1775372c114ce70ed0626e0c3b5ad6d97a21b9
Santos-Pazo <69361240+Santos-Pazo@users.noreply.github.com>
2021-04-26 12:05:21 -0300

    arreglo en app. js

--- proyectoIntegrador/routes/users.js
+++ proyectoIntegrador/routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var usersController = require('../controllers/usersController')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/register', usersController.login)
+router.get('/register', usersController.register)
 router.get('/login', usersController.login)
 
 module.exports = router;
