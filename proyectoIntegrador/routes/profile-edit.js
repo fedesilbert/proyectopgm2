bb2462776bf78a647caeff354efe5518758137d1
Santos-Pazo <69361240+Santos-Pazo@users.noreply.github.com>
2021-04-26 13:35:26 -0300

    mas correcciones

--- proyectoIntegrador/routes/profile-edit.js
+++ proyectoIntegrador/routes/profile-edit.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var profileEditController = require('../controllers/profileEditController')
 
+router.get('/', profileEditController.profileEdit)
 
 module.exports = router;
--- proyectoIntegrador/routes/resultadoBusqueda.js
+++ proyectoIntegrador/routes/resultadoBusqueda.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var resultadoBusquedaController = require('../controllers/resultadoBusquedaController')
 
+router.get('/', resultadoBusquedaController.resultadoBusqueda)
 
 module.exports = router;
